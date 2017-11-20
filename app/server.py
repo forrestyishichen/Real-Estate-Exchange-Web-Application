59fc8c2720aa0c29fbe169466030c88309b84e64
forrestyisshichen <forrestyschen@gmail.com>
2017-11-19 22:44:08 -0800

    remove old folder, add v1.0
    login, register, main page, session, routing

--- app/server.py
+++ app/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, session, g, abort, redirect, url_for, render_template, flash, current_app, jsonify
+from flask import Flask, request, session, escape, g, abort, redirect, url_for, render_template, flash, current_app, jsonify
 from flaskext.mysql import MySQL
 import json
 
@@ -21,7 +21,8 @@ Index
 
 @app.route('/')
 def index():
-    return current_app.send_static_file('index.html')
+    return render_template('login.html')
+    # return current_app.send_static_file('index.html')
 
 '''
 Static page
@@ -32,26 +33,13 @@ def static_page(page_name):
     return current_app.send_static_file('%s.html' % page_name)
 
 '''
-Old output
-'''
-
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    cursor = mysql.connect().cursor()
-    cursor.execute("SELECT * from User")
-    # mydata = json.dumps(cursor.fetchall())
-    # mydata = [dict(username=row[0], password=row[1]) for row in cursor.fetchall()]
-    mydata = cursor.fetchall()
-    return render_template('hello.html', name=mydata)
-
-'''
-New output
+Market Report with Search Page!
 '''
 
 @app.route('/show')
 def show_entries():
-    cursor = mysql.connect().cursor()
+    conn = mysql.connect()
+    cursor = conn.cursor()
     cursor.execute("SELECT * from User")
     # cur = g.db.execute('select title, text from entries order by id desc')
     entries = [dict(title=row[0], text=row[1]) for row in cursor.fetchall()]
@@ -72,7 +60,7 @@ def add_entry():
     return redirect(url_for('show_entries'))
 
 '''
-New login
+Login
 '''
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -88,7 +76,8 @@ def login():
             error = 'Invalid username and password'
         else:
             session['logged_in'] = True
-            flash('You were logged in')
+            session['username'] = username
+            flash('Logged in as %s' % escape(session['username']))
             return redirect(url_for('show_entries'))
     return render_template('login.html', error=error)
 
@@ -112,20 +101,21 @@ def register():
             cursor.execute('''INSERT INTO User (userName, password) VALUES(%s, %s)''', (username, password))
             conn.commit()
             session['logged_in'] = True
-            flash('You were registered')
+            session['username'] = username
+            flash('You were registered, %s' % escape(session['username']))
             return redirect(url_for('show_entries'))
     return render_template('register.html', error=error)
 
 '''
-Login out
+Logout
 '''
 
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
+    session.pop('username', None)
     flash('You were logged out')
     return redirect(url_for('show_entries'))
-
 
 '''
 Single user page
@@ -141,46 +131,10 @@ def show_user_profile(username):
     else:
      return data
 
-# @app.route('/user/<username>')
-# def show_user_profile(username):
-#     # show the user profile for that user
-#     return 'User %s' % username
-
-# @app.route('/post/<int:post_id>')
-# def show_post(post_id):
-#     # show the post with the given id, the id is an integer
-#     return 'Post %d' % post_id
-
-'''
-Old Login Section
-'''
-@app.route("/Authenticate", methods=['GET'])
-def Authenticate():
-    username = request.args.get('UserName')
-    password = request.args.get('Password')
-    cursor = mysql.connect().cursor()
-    cursor.execute("SELECT * from User where Username='" + username + "' and Password='" + password + "'")
-    data = cursor.fetchone()
-    if data is None:
-     return current_app.send_static_file('loginfailed.html')
-    else:
-     return current_app.send_static_file('loginok.html')
-
-@app.route("/Newuseradder")
-def Newuseradder():
-    username = request.args.get('UserName')
-    password = request.args.get('Password')
-    print(username)
-    print(password)
-    conn = mysql.connect()
-    cursor = conn.cursor()
-    cursor.execute('''INSERT INTO User (userName, password) VALUES(%s, %s)''', (username, password))
-    conn.commit()
-    return redirect(url_for('index'))
-
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('page_not_found.html'), 404
+    return "404 page_not_found" , 404
+    # return render_template('page_not_found.html'), 404
 
 if __name__ == '__main__':
     app.debug = True
